7d32ebda818cd0725d464e974fcd3bbeedd5c4ae
Chris Cardle <cdcardle@gmail.com>
2018-07-20 18:58:45 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -24,7 +24,7 @@ class Boat < ActiveRecord::Base
   end
 
   def self.sailboats
-    where(:classifications).include(name: "ailboat")
+    where(:classifications).include(name: "Sailboat")
   end
 
   def self.with_three_classifications
