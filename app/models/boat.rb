4ec8fb6dd7ab3f126b5859dc074ffcb3d9b00c55
Chris Cardle <cdcardle@gmail.com>
2018-07-20 18:28:35 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,12 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    boats = []
+    Boat.all.each do |boat|
+      boats << boat if boat.id <6
+    end
+    boats
+  end
 end
