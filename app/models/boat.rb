7050f9e9d1afb9252955491fe5566941a10fe5c5
Chris Cardle <cdcardle@gmail.com>
2018-07-20 19:10:50 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -24,7 +24,7 @@ class Boat < ActiveRecord::Base
   end
 
   def self.sailboats
-    self.joins(:classifications).select('classifications.name').where(name: "Sailboat")
+    self.joins(:classifications).select('classifications.name')
   end
 
   def self.with_three_classifications
