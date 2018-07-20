f0aa8d720a008ccbadeb187d4b2ec68a03cdc16d
Chris Cardle <cdcardle@gmail.com>
2018-07-20 22:28:14 +0000

    automatically backed up by learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -2,10 +2,10 @@ class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
 
-  # def self.my_all
-  #
-  # end
-  #
+  def self.my_all
+    all.uniq
+  end
+
   # def self.longest
   #
   # end
